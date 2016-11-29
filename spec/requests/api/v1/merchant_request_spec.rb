df5ba3d7fc5f09ef9cb8166864949ad1d765c345
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-11-28 19:05:05 -0700

    Merchant index created

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -12,4 +12,13 @@ describe 'merchant endpoint' do
       expect(merchants.count).to eq(3)
     end
   end
+  # context "GET a merchant's revenue" do
+  #   it "returns the revenue for a merchant" do
+  #     create_list(:merchant, 3)
+  #
+  #     get "/api/v1/merchants/#{Merchant.first.id}/revenue"
+  #
+  #     expect(response).to be_success
+  #   end
+  # end
 end
