d8ade8fb084850f57278255bed2d3e3b3575a844
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-11-28 20:37:40 -0700

    Index and show for merchants created

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.permit()
+  end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -12,6 +12,18 @@ describe 'merchant endpoint' do
       expect(merchants.count).to eq(3)
     end
   end
+
+  context 'GET a merchant' do
+    it 'returns a merchant' do
+      create_list(:merchant, 3)
+      get "/api/v1/merchants/#{Merchant.first.id}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant.first[1]).to eq(Merchant.first.id)    
+    end
+  end
   # context "GET a merchant's revenue" do
   #   it "returns the revenue for a merchant" do
   #     create_list(:merchant, 3)
