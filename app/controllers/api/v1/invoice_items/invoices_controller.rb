580d01bfadd8a4616700411513b4207e4ad74b28
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-11-30 08:23:44 -0700

    Invoice Items relationships added

--- app/controllers/api/v1/invoice_items/invoices_controller.rb
+++ app/controllers/api/v1/invoice_items/invoices_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::InvoiceItems::InvoicesController < ApplicationController
 
   def show
-    byebug
-    render json: InvoiceItem.find_by(invoice_items_params)
+    render json: InvoiceItem.find(params[:id].to_i).invoice
   end
 end
--- spec/requests/api/v1/invoice_items_relation_request_spec.rb
+++ spec/requests/api/v1/invoice_items_relation_request_spec.rb
@@ -11,12 +11,26 @@ describe 'invoice_items relation endpoint' do
   context 'GET invoice through invoice_items' do
     it 'returns an invoice' do
       create_list(:invoice_item, 3, item_id: Item.first.id ,invoice_id: Invoice.first.id)
-      get "/api/v1/invoice_items/:id/invoice"
 
-      invoice_items = JSON.parse(response.body)
+      get "/api/v1/invoice_items/#{InvoiceItem.first.id}/invoice"
+
+      invoice = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(invoice_items.count).to eq(3)
+      expect(invoice["id"]).to eq(Invoice.first.id)
+    end
+  end
+  context "GET item through invoice_items" do
+    it 'returns an item' do
+      create_list(:invoice_item, 3, item_id: Item.first.id ,invoice_id: Invoice.first.id)
+
+      get "/api/v1/invoice_items/#{InvoiceItem.first.id}/item"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["id"]).to eq(Item.first.id)
+
     end
   end
 end
