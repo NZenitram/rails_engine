8c278f1d743477d3af327e1eb6709c6f051019ed
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-11-29 17:05:07 -0700

    Add test for relationship api

--- spec/requests/api/v1/invoices_relation_request_spec.rb
+++ spec/requests/api/v1/invoices_relation_request_spec.rb
@@ -8,13 +8,31 @@ describe 'invoice endpoint' do
   context 'GET transactions associated with invoice' do
     it 'returns a list of all transactions' do
       create_list(:invoice, 3, customer_id: Customer.first.id, merchant_id: Merchant.first.id)
+      create_list(:transaction, 3, invoice_id: Invoice.first.id)
+
 
       get "/api/v1/invoices/#{Invoice.first.id}/transactions"
 
-      invoices = JSON.parse(response.body)
+      transactions = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(invoices.count).to eq(3)
+      expect(transactions.count).to eq(3)
+    end
+  end
+
+  context 'GET transactions associated with invoice' do
+    it 'returns a list of all transactions' do
+      create_list(:invoice, 3, customer_id: Customer.first.id, merchant_id: Merchant.first.id)
+      create_list(:transaction, 3, invoice_id: Invoice.first.id)
+      create_list(:invoice_item, 3, invoice_id: Invoice.first.id)
+
+
+      get "/api/v1/invoices/#{Invoice.first.id}/invoice_items"
+
+      transactions = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transactions.count).to eq(3)
     end
   end
 end
