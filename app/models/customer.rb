2369e849420f64faf5566bd702f3dc6aa99f530b
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-01 10:34:10 -0700

    Move favorite merchant to two lines

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
   has_many :merchants, through: :invoices
 
     def favorite_merchant
-      merchants.joins(:transactions).merge(Transaction.successful).group(:id, :name).order("count(merchants.id) DESC").first
+      merchants.joins(:transactions).merge(Transaction.successful)
+      .group(:id, :name).order("count(merchants.id) DESC").first
   end
 end
