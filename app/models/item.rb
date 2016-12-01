81bf0e84c38282ecc4ef5186d92a2c85196bde6d
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-01 10:35:16 -0700

    Add method for items with most revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,12 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
 
-  has_many :invoices, through: :invoice_items
+  has_many :invoices, {:through=>:invoice_items, :source=>"invoice"}
 
-
+  def self.most_revenue(num)
+    joins(invoices: [:transactions])
+      .merge(Transaction.successful).group(:id)
+      .order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
+      .first(num)
+  end
 end
