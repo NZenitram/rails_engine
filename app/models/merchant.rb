f767708d433721eb911307785850e9b0053f2411
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-01 10:36:19 -0700

    Remove unused method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
-
+  has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
   def total_revenue(date = nil)
@@ -9,5 +9,4 @@ class Merchant < ApplicationRecord
       .where(transactions: {result: "success"})
       .sum("invoice_items.quantity * invoice_items.unit_price")
   end
-
 end
