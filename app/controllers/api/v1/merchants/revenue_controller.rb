fd0ac13919542c3ad40643111f427fc2b9a9bc34
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-01 14:42:44 -0700

    Revenue display fixed

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: Merchant.find(params[:id]).total_revenue(params[:date])
+    render json: Merchant.find(params[:id]).total_revenue(params[:date]), serializer: MerchantrevenueSerializer
   end
 
 end
